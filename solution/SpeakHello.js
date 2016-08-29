70431cb871498340f858a2c9d27570f1a92354d6
Kateryna007 <katerynakozh@gmail.com>
2016-08-29 20:00:48 +0300

    Update SpeakHello.js

--- solution/SpeakHello.js
+++ solution/SpeakHello.js
@@ -1,7 +1,11 @@
-  var yaakovGreeter = {};
-  yaakovGreeter.name = "Yaakov";
-  yaakovGreeter.sayHello = function () {
-  console.log("Hello " + yaakovGreeter.name);
-  }
-
+(function (window) {
+var helloSpeaker = {};
+//byeSpeaker.name =("Yaakov", "John", "Jen", "Jason", "Paul", 	"Frank", "Larry", "Paula", "Laura", "Jim");
+var speakWord = "Hello ";
+helloSpeaker.speak = function (name) {
+	 console.log(speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
+ })(window);
+ 
  
