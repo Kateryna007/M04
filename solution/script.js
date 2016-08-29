d8ba91f1ae7cf4f7e3af70eeea656b607b6160ff
Kateryna007 <katerynakozh@gmail.com>
2016-08-29 20:00:18 +0300

    Update script.js

--- solution/script.js
+++ solution/script.js
@@ -1,5 +1,17 @@
- yaakovGreeter.sayHello();
- yaakovGreeter.sayBye()
+(function () {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", 	"Frank", "Larry", "Paula", "Laura", "Jim"];
+	//	var speakWord = "Hello ";
+	//	helloSpeaker.speak = function () {}
+for (var i = 0; i < names.length; i++){
+	 var firstLetter = names[i].charAt(0).toLowerCase();
+ if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {(firstLetter != 'j') 
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})(); 
 
 
 
